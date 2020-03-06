a4597fdec8037d25960b8c46839b54de2abc996e
PaulChanHK <19663576+PaulChanHK@users.noreply.github.com>
2020-03-07 02:43:37 +0800

    run - RW umask and rm minidlna.pid
    Signed-off-by: PaulChanHK <19663576+PaulChanHK@users.noreply.github.com>

--- run/entrypoint.sh
+++ run/entrypoint.sh
@@ -6,4 +6,10 @@ if [ -n "$(head -n1 /CHANGES | grep -E 'original$')" ]; then
   sed -ri 's/^(media_dir=[AVP]+,[^,]+).*/\1/p' /conf/minidlna.conf
 fi
 
+# allow RW on all created files
+umask 0000
+
+# remove pid file to ensure daemon can be started
+rm -f /var/run/minidlna/minidlna.pid
+
 exec /usr/local/sbin/minidlnad "$@"
